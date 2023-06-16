2bbb37011183eeaffb7178a9608a05c6155b3c0c
Mari Félix <marianafelixdelima7@gmail.com>
2023-06-16 17:27:31 -0300

    UPDATE Mundo2

--- Mundo 2/ex037.py
+++ Mundo 2/ex037.py
@@ -1,7 +1,17 @@
-'''
-Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher
-qual será a base de conversão:
-1) Binário
-2) Octal
-3) Hexadecimal
-'''
+lista = (1, 2, 3)
+num = int(input('Digite um número: '))
+n_base = int(input('''
+Para qual base será convertido?
+[1] BINÁRIO
+[2] OCTAL
+[3] HEXADECIMAL
+'''))
+
+if n_base not in lista:
+    print('ERRO! Digite opções disponíveis.')
+elif n_base == lista[0]:
+    print(f'BINÁRIO: {(bin(num)[2:].upper())}')
+elif n_base == lista[1]:
+    print(f'OCTAL: {oct(num)[2:].upper()}')
+elif n_base == lista[2]:
+    print(f'HEXADECIMAL: {hex(num)[2:].upper()}')
--- Mundo 2/ex038.py
+++ Mundo 2/ex038.py
@@ -2,6 +2,15 @@
 Escreva um número que compare dois núemros inteiros e compare-os, mostrando na
 tela uma mensagem: 
 - O primeiro valor é o maior
-- O segudno vallor é o maior
+- O segundo vallor é o maior
 - Não existe valor maior, os dois são iguais
 '''
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+if n1 > n2:
+    print('O primeiro é maior que o segundo')
+elif n2 > n1:
+    print('O segundo é maior que o primeiro')
+else:
+    print('Não existe MAIOR. Os dois são IGUAIS.')
